aeb7c149fa4ba4c796166704f09973d8749f62b5
csmcorporatama@gmail.com <indorent10>
2022-03-07 11:55:47 +0700

    dsadas

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -31,7 +31,7 @@
 
     @include('layout.style')
 
-    <link rel="icon" type="image/x-icon" href="{{ URL::to('/') }}/images/logo_1.ico">
+    <link rel="icon" type="image/x-icon" href="{{ URL::to('/') }}/favicon.ico">
     <title>Edy Prasetyo</title>
 </head>
 
