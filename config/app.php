411426c31e277cd8749b12bb6d1617a22413966e
csmcorporatama@gmail.com <indorent10>
2022-03-09 16:43:17 +0700

    dsadas

--- config/app.php
+++ config/app.php
@@ -175,8 +175,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        \MigrationsGenerator\MigrationsGeneratorServiceProvider::class
+        App\Providers\RouteServiceProvider::class
     ],
 
     /*
